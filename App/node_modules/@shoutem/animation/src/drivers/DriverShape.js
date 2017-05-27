6cdbaa4e987dcf3a16a8685a36e0274b978e41cb
Nhat Do <nhatdo996@gmail.com>
2017-05-27 16:53:04 +0700

    source

---
+++ App/node_modules/@shoutem/animation/src/drivers/DriverShape.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Animated } from 'react-native';
+
+export const DriverShape = React.PropTypes.shape({
+  value: React.PropTypes.instanceOf(Animated.Value),
+  interpolate: React.PropTypes.func,
+});
